dbc1bdec9c4fce87e861ed76309ea2aef63425f0
Jared M. Peterson <developer@peteyinc.com>
2018-03-20 17:04:43 -0600

    fix templates and styles

--- track_hymn/controllers/home_controller.py
+++ track_hymn/controllers/home_controller.py
@@ -1,25 +1,12 @@
 import pyramid_handlers
 from track_hymn.controllers.base_controller import BaseController
-from track_hymn.infrastructure.suppressor import suppress
+
+
+# from track_hymn.infrastructure.suppressor import suppress
 
 
 class HomeController(BaseController):
 
     @pyramid_handlers.action(renderer='templates/home/index.pt')
     def index(self):
-        return {
-            'value': 'HOME'
-        }
-
-    def about(self):
-        pass
-
-    @pyramid_handlers.action(renderer='templates/home/contact.pt')
-    def contact(self):
-        return {
-            'value': 'CONTACT'
-        }
-
-    @suppress
-    def testing(self):
-        print("testing")
+        return {}
